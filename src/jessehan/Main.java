1e794a8ac4da546dc5bd157567300de932abb368
thanosfromfortnite <43213177+thanosfromfortnite@users.noreply.github.com>
2018-09-20 11:29:30 -0400

    Update Main.java

--- src/jessehan/Main.java
+++ src/jessehan/Main.java
@@ -2,7 +2,8 @@ package jessehan;
 
 public class Main {
     public static void main(String[] args) {
-	    System.out.println(JHanlib.isPalindrome("tip your halal food people"));
+	    //System.out.println()
+	            JHanlib.multiplicationTable(8, 3);
     }
 }
 /*
@@ -18,5 +19,12 @@ dateStr();
 "05/09/2001" -> "09 - 05 - 2001"
 "not a date" -> "The date was not put in correctly..."
 
+sumUpTo();
+5 -> 15
+14 -> 105
+
+multiplicationTable();
+5, 4 -> 0, 5, 10, 15
+8, 3 -> 0, 8, 16
 
  */
