12e3d2ef4bc20d353e4e9b98cbc73323e588b5ee
thanosfromfortnite <43213177+thanosfromfortnite@users.noreply.github.com>
2018-09-20 11:12:36 -0400

    Add files via upload

--- src/jessehan/Main.java
+++ src/jessehan/Main.java
@@ -2,6 +2,21 @@ package jessehan;
 
 public class Main {
     public static void main(String[] args) {
-	    System.out.println(JHanlib.dateStr("05/09/2001"));
+	    System.out.println(JHanlib.isPalindrome("tip your halal food people"));
     }
 }
+/*
+Tests
+
+isPalindrome();
+"racecar" -> true
+"aaaaaa" -> true
+"tip your halal food people" -> false
+
+
+dateStr();
+"05/09/2001" -> "09 - 05 - 2001"
+"not a date" -> "The date was not put in correctly..."
+
+
+ */
